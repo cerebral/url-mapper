38c981a84955c1abbf021072efec03c65709eb95
christianalfoni <christianalfoni@gmail.com>
2015-08-26 17:18:59 +0200

    reoved logging

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var utils = require('./src/utils.js');
 
 module.exports = function (url, routes) {
 
-  console.log(url);
     // This logic should probably be better, has to Handle
     // /#/foo, #/foo, /foo, /foo/, /#/foo/, #/foo/
     var path = url.replace(location.origin, '').replace('#', '').replace('#', '').split('');
@@ -11,7 +10,6 @@ module.exports = function (url, routes) {
       path.pop();
     }
     path = path.join('');
-    console.log('path', path);
     var route = utils.findMatchingRoute(routes, path);
     var params = utils.parseParams(route, path);
 
