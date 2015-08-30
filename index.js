5fce9fa11b0a611cdc963da34cf9cdffd35a7e3b
Garth Williams <garth@wi.llia.ms>
2015-08-30 16:31:15 +0200

    Use path-to-regexp module to parse the path
    Fixes url encoding issues
    Also added support for * catch all

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 'use strict';
-var utils = require('./src/utils.js');
+var qs = require('qs');
+var pathtoRegexp = require('path-to-regexp');
+
+var cache = {};
+
+function isMatch(re, path, keys) {
+  var match = re.exec(decodeURIComponent(path));
+  if (!match) { return null; }
+
+  var params = {};
+
+  for (var i = 1; i < match.length; ++i) {
+    var key = keys[i - 1];
+    var value = decodeURIComponent(match[i].replace(/\+/g, ' '));
+    if (value !== undefined || !(hasOwnProperty.call(params, key.name))) {
+      params[key.name] = value;
+    }
+  }
+
+  return params;
+};
 
 module.exports = function (url, routes) {
 
@@ -13,13 +33,26 @@ module.exports = function (url, routes) {
       path.shift();
     }
     path = path.join('');
-    var route = utils.findMatchingRoute(routes, path);
-    var params = utils.parseParams(route, path);
-    var queryString = location.search;
 
-    if (queryString) {
-      var query = utils.parseQueryString(queryString.substr(1));
+    var params = null;
+    var route = null;
+    for (route in routes) {
+      if (!cache[route]) {
+        var keys = [];
+        var re = pathtoRegexp(route === '*' ? '(.*)' : route, keys)
+        cache[route] = {
+          keys: keys,
+          re: re
+        }
+      }
+      params = isMatch(cache[route].re, path, cache[route].keys);
+      if (params) {
+        break;
+      }
     }
+
+    var queryString = location.search;
+    var query = queryString ? qs.parse(queryString.substr(1)) : {};
 
     routes[route]({
       path: path,
