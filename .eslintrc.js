d233353eb038c87b14ec6e828ba2e33d9b03a2b7
Aleksei Gurianov <gurianov@gmail.com>
2021-04-15 16:54:05 +0400

    chore: setup eslint, prettier, commitizen, commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+  },
+  extends: ['eslint:recommended', 'standard', 'plugin:prettier/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2015,
+  },
+  rules: {
+    'no-var': 'off',
+    'prettier/prettier': 'error',
+  },
+}
